57d60e2a977165ea5f1ced31de40047c6d35b1b4
jufajeror1-afk <jufajeror14@gmail.com>
2026-04-14 18:49:58 +0800

    版本 ： 1.0.0

---
+++ account-service/src/main/java/net/zicai/mapper/AccountMapper.java
@@ -0,0 +1,16 @@
+package net.zicai.mapper;
+
+import net.zicai.model.AccountDO;
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+
+/**
+ * <p>
+ * 用户表，支持微信登录 Mapper 接口
+ * </p>
+ *
+ * @author 紫菜,
+ * @since 2026-04-14
+ */
+public interface AccountMapper extends BaseMapper<AccountDO> {
+
+}
