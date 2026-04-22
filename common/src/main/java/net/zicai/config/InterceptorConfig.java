e19e9456ed1fe589e97e6fc7adb450c01fc73647
jufajeror1-afk <jufajeror14@gmail.com>
2026-04-22 17:57:42 +0800

    feat(config): 新增InterceptorConfig和AccountLoginInterceptor类
    - 在common模块的net.zicai.config包中添加InterceptorConfig类
    - 在common模块的net.zicai.interceptor包中添加AccountLoginInterceptor类
    - 两个类均为基础骨架，尚未实现具体功能
    - 添加类注释并标注作者和创建日期

---
+++ common/src/main/java/net/zicai/config/InterceptorConfig.java
@@ -0,0 +1,9 @@
+package net.zicai.config;
+
+/**
+* @author 王镝
+* @date 20260422 
+**/
+
+public class InterceptorConfig {
+}
---
+++ common/src/main/java/net/zicai/interceptor/AccountLoginInterceptor.java
@@ -0,0 +1,9 @@
+package net.zicai.interceptor;
+
+/**
+* @author 王镝
+* @date 20260422 
+**/
+
+public class AccountLoginInterceptor {
+}
