d32e9ab2b67616a3710eed8b575a448ca6762282
jufajeror1-afk <jufajeror14@gmail.com>
2026-05-11 20:39:35 +0800

    feat(product-service): 添加订单相关请求类和配置类
    - 新增 ProductOrderMQConfig 配置类
    - 新增 BenefitOrderCreateReq 请求类（空类）
    - 在 PackageOrderCreateReq 类中添加 BenefitOrderCreateReq 并增加字段及校验
    - benefitId 字段不能为空，类型为 Long
    - porchaseCount 字段不能为空且最小值为1
    - payType 字段不能为空，类型为 String
    - discount 字段默认为1，类型为 BigDecimal

---
+++ product-service/src/main/java/net/zicai/config/ProductOrderMQConfig.java
@@ -0,0 +1,9 @@
+package net.zicai.config;
+
+/**
+ * @author wangdi
+ * @date 2026/5/11 20:30
+ * @description 
+ */
+public class ProductOrderMQConfig {
+}
---
+++ product-service/src/main/java/net/zicai/controller/req/BenefitOrderCreateReq.java
@@ -0,0 +1,9 @@
+package net.zicai.controller.req;
+
+/**
+ * @author wangdi
+ * @date 2026/5/11 15:48
+ * @description 
+ */
+public class BenefitOrderCreateReq {
+}
