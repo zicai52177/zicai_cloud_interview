591bfcbebff33a0562697205bf9b73e5a12eb21b
jufajeror1-afk <jufajeror14@gmail.com>
2026-05-25 13:06:54 +0800

    feat(util): 增加JSON与对象列表相互转换功能
    - 引入Jackson的ObjectMapper进行JSON操作
    - 实现json字符串转对象列表的方法json2List
    - 实现对象转json字符串的方法obj2Json
    - 扩展SpringBeanUtil工具类功能，支持JSON转换
    - 在转换失败时抛出运行时异常提示错误原因

--- common/src/main/java/net/zicai/util/SpringBeanUtil.java
+++ common/src/main/java/net/zicai/util/SpringBeanUtil.java
@@ -1,15 +1,20 @@
 package net.zicai.util;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.BeanUtils;
 
 import java.util.ArrayList;
 import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 
 /**
- * Spring Bean 工具类，提供对象属性拷贝功能
+ * Spring Bean 工具类，提供对象属性拷贝和JSON转换功能
  */
 public class SpringBeanUtil {
+
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
 
     public static <T> T copyProperties(Object source, Class<T> targetClass) {
         if (source == null) {
@@ -33,5 +38,27 @@ public class SpringBeanUtil {
             result.add(copyProperties(source, targetClass));
         }
         return result;
+    }
+
+    public static <T> List<T> json2List(String json, Class<T> clazz) {
+        if (json == null || json.isEmpty()) {
+            return Collections.emptyList();
+        }
+        try {
+            return OBJECT_MAPPER.readValue(json, OBJECT_MAPPER.getTypeFactory().constructCollectionType(List.class, clazz));
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("JSON转List失败", e);
+        }
+    }
+
+    public static String obj2Json(Object obj) {
+        if (obj == null) {
+            return null;
+        }
+        try {
+            return OBJECT_MAPPER.writeValueAsString(obj);
+        } catch (JsonProcessingException e) {
+            throw new RuntimeException("对象转JSON失败", e);
+        }
     }
 }
